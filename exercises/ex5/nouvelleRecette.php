d265be8a9e7ff5d1be57e5c3b97d96248016cde6
NGUYEN Minh Kha <pro.nguyenminhkha@gmail.com>
2022-10-19 14:12:29 +0200

    exercise 6

--- exercises/ex5/nouvelleRecette.php
+++ exercises/ex5/nouvelleRecette.php
@@ -1,23 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php $TITLE = "Nouvelle recette"; ?>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
-        integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
-        crossorigin="anonymous" />
-
-    <link rel="stylesheet" href="./includes/css/style.css" />
-    <link rel="stylesheet" href="./includes/css/footer.css" />
-    <link rel="stylesheet" href="./includes/css/form.css" />
-    <title>Nouvelle recette</title>
-</head>
+<? include('./includes/head.php') ?>
 
 <body>
 
@@ -29,9 +15,7 @@
     </br>
     <? include('./includes/footer.php') ?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js"
-        integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous">
-    </script>
+    <? include('./includes/bootstrapScript.php') ?>
 </body>
 
 </html>
---
+++ exercises/ex5/recette.php
@@ -0,0 +1,47 @@
+<?
+/*
+En reprenant le code du site de recette de cuisine,
+dans la page recette.php, ajouter le code permettant
+d'afficher une recette.
+ATTENTION ! Vous devez impérativement tester et
+valider l'identifiant passé en Query string
+*/
+?>
+
+<?php
+
+    if (isset($_GET['id'])) { 
+        $id = filter_var($_GET['id'], FILTER_VALIDATE_INT);
+        if ($id) {
+include('./includes/content.php');
+
+        ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php $TITLE = "Page de la recette n° " . $id-1; ?>
+<? include('./includes/head.php') ?>
+
+<body>
+    <? include('./includes/navbar.php') ?>
+    </br>
+    <? echo PrettyRecipe($recettes[$id-1]) ?>
+    </br>
+    <? include('./includes/footer.php') ?>
+
+    <? include('./includes/bootstrapScript.php') ?>
+
+</body>
+
+</html>
+
+<?php
+
+        }
+        else {
+            echo '<h1> false id </h1>';
+        }
+    } else {
+        echo '<h1> id not found </h1>';
+}
+?>
