9722504a64a972d59bd1a3517fa2c52ffd017923
NGUYEN Minh Kha <pro.nguyenminhkha@gmail.com>
2022-10-19 16:45:14 +0200

    add email & password validation

--- exercises/ex5/connexion.traitement.php
+++ exercises/ex5/connexion.traitement.php
@@ -2,27 +2,35 @@
 
 include('./includes/users.php');
 
-$email = $_POST['email'];
-$password = $_POST['password'];
-$user = array();
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+    $password = $_POST['password'];
+    $user = null;
 
-foreach ($utilisateurs as $key => $value) {
-    if ($email == $value['email'] && $password == $value['password']) {
-        $user = $value;
+    if ($email) {
+        include('./includes/users.php');
+        foreach ($utilisateurs as $key => $value) {
+            if ($email === $value['email'] && $password === $value['password']) {
+                $user = $value;
+            }
+        }
+
+        if (is_null($user)) {
+?>
+            <h1> User not found </h1>
+        <?php
+        } else {
+        ?>
+            <h1> Bienvenue <? echo $user['nom'] ?>
+                <? echo $user['prenom'] ?></h1>
+
+<?php
+        }
+    } else {
+        echo '<h1> false email </h1>';
     }
-}
-
-if (is_null($user)) {
-?>
-    <h1> User not found </h1>
-<?php
 } else {
-?>
-    <h1> Bienvenue <? echo $user['nom'] ?>
-        <? echo $user['prenom'] ?></h1>
-
-<?php
+    echo '<h1> email or password not found </h1>';
 }
-
 
 ?>
