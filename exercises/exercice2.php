66f6624b6911c1c9d5d3e56df26a524cf98b7837
NGUYEN Minh Kha <pro.nguyenminhkha@gmail.com>
2022-10-22 00:13:08 +0200

    add exs

--- exercises/exercice2.php
+++ exercises/exercice2.php
@@ -1,6 +1,7 @@
 <!--
 Créer une fichier "exercice2.php" et placer le code
 de l'exercice.
+
 Avec le tableaux suivant :
 -->
 <?php
@@ -13,7 +14,9 @@ $eleve = [
 ?>
 <!--
 Dans une balise h1 afficher le nom et le prenom de l'éléve
+
 Ensuite dans une balise h3 afficher l'age de l'éléve
+
 Pour finir dans une balise p afficher la classe de l'éléve
 -->
 
@@ -24,15 +27,24 @@ Pour finir dans une balise p afficher la classe de l'éléve
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Exercice 2</title>
 </head>
 
 <body>
-    <?php
-    echo '<h1>' . $eleve['nom'] ." ". $eleve['prenom'] . '</h1>';
-    echo '<h3>' . $eleve['age'] . '</h3>';
-    echo '<p>'. $eleve['classe'] .'</p>';
-    ?>
+    <!-- 
+    En php il éxiste des "short open tag", des balises ouvrante
+    et fermante php plus simple à écrire.
+
+    Par éxemple au lieu d'écrire :
+    <?php echo 'test'; ?>
+
+    Vous pouvez écrire simplement :
+    <?= 'test' ?>
+    -->
+
+    <h1><?= $eleve['nom'] ?> <?= $eleve['prenom'] ?></h1>
+    <h3>Age : <?= $eleve['age'] ?></h3>
+    <p>Classe : <?= $eleve['classe'] ?></p>
 </body>
 
 </html>
--- exercises/exercice4.php
+++ exercises/exercice4.php
@@ -1,6 +1,7 @@
 <!--
 Créé un fichier 'exercice4.php" et placé le code
 de l'exerice suivant :
+
 Avec le tableaux suivant :
 -->
 <?php
@@ -20,19 +21,27 @@ $eleve = [
 <!--
 Afficher dans une balise h1 :
 Fiche de l'éléve (nom) (prenom)
+
 Afficher dans une balise h2 "Information sur (nom) (prenom)"
+
 À la suite, en utilisant les balises html dd, dt, dl Afficher
 l'age et la class de l'éléve
+
 Afficher à la suite une balise h3 "Professeur principal"
+
 À la suite, à l'aide des balises dd, dt, dl afficher
 le nom, prenom et matiere du professeur principal.
+
 Afficher ensuite dans une balise h2 "Notes"
+
 À la suite, en utilisant une boucle et une balise ul,
 afficher toutes les notes de l'éléve.
+
 BONUS :
 - Mettre la note en vert si au dessus de 15
 - Mettre la note en orange si entre 10 et 15
 - Mettre la note en rouge si en dessous de 10
+
 Indice, vous pouvez créer et attacher une feuille de style css
 de la même manière que en HTML :)
 -->
@@ -44,36 +53,40 @@ de la même manière que en HTML :)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./exercice4.css" />
+    <title>Exercice 4</title>
 </head>
 
 <body>
-    <?php
-    echo '<h1>' . "Fiche de l'éléve " . $eleve['nom'] ." ". $eleve['prenom'] . '</h1>';
-    echo '<dl>';
-    echo '<dt>' . $eleve['age'] . '</dt>';
-    echo '<dd>'. $eleve['classe'] .'</dd>';
-    echo '</dl>';
-    echo '<h3> Professeur principal </h3>';
-    echo '<dl>';
-    echo '<dt>' . $eleve['profPrincipal']['nom'] . " ". $eleve['profPrincipal']['prenom'] . '</dt>';
-    echo '<dd>'. " matière " . $eleve['profPrincipal']['matiere'] .'</dd>'; 
-    echo '</dl>';
-    echo '<h2> Notes </h2>';
-    echo '<ul>';
-    foreach ($eleve['notes'] as $index => $note) {
-        if($note >= 15){
-            echo '<li style="color:green" > note '. $index . " : " . $note .'</li>';
-        } else if ($note < 15 && $note >= 10){
-            echo '<li style="color:orange" > note '. $index . " : " . $note .'</li>';
-        } else {
-            echo '<li style="color:red" > note '. $index . " : " . $note .'</li>';
-        }
-        
-        
-    }
-    echo '</ul>';
-    ?>
+    <h1>Fiche de l'élève <?= $eleve['nom'] ?> <?= $eleve['prenom'] ?></h1>
+
+    <h2>Information sur <?= $eleve['nom'] ?> <?= $eleve['prenom'] ?></h2>
+
+    <dl>
+        <dt>Age</dt>
+        <dd><?= $eleve['age'] ?></dd>
+        <dt>Classe</dt>
+        <dd><?= $eleve['classe'] ?></dd>
+    </dl>
+
+    <h3>Professeur Principal</h3>
+
+    <dl>
+        <dt>Nom</dt>
+        <dd><?= $eleve['profPrincipal']['nom'] ?></dd>
+        <dt>Prénom</dt>
+        <dd><?= $eleve['profPrincipal']['prenom'] ?></dd>
+        <dt>Matière</dt>
+        <dd><?= $eleve['profPrincipal']['matiere'] ?></dd>
+    </dl>
+
+    <h2>Notes</h2>
+
+    <ul>
+        <? foreach ($eleve['notes'] as $index => $note) : ?>
+            <li class="<? if ($note > 15) : ?>green<? elseif ($note >= 10) : ?>orange<? else : ?>red<? endif ?>">Note n°<?= $index + 1 ?> : <?= $note ?></li>
+        <? endforeach ?>
+    </ul>
 </body>
 
 </html>
